af5044c0295d56307bd9ef7a3853a469ec6bf890
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2025-01-02 22:50:48 +0100

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
