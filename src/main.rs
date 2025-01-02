d65633d1ad9e4fda55c9d358d59696abe2a83389
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2025-01-02 23:17:17 +0100

    refactor concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread::{self},
 };
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    println!("accepted new connection");
+
+    loop {
+        let read_count = stream.read(&mut buf).unwrap();
+        if read_count == 0 {
+            break;
+        }
+        stream.write_all(b"+PONG\r\n").unwrap();
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-
-                loop {
-                    println!("accepted new connection");
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
