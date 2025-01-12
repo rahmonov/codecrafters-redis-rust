2c9f2b17ec5a068e3de5af1ef9db8bfb5c29432c
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2025-01-12 19:01:38 +0100

    implement parsing redis request

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use resp::Value;
 use tokio::net::{TcpListener, TcpStream};
+use anyhow::Result;
 
-async fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 512];
-    println!("accepted new connection");
-
-    loop {
-        let read_count = stream.read(&mut buf).await.unwrap();
-        if read_count == 0 {
-            break;
-        }
-        stream.write_all(b"+PONG\r\n").await.unwrap();
-    }
-}
+mod resp;
 
 #[tokio::main]
 async fn main() {
@@ -24,5 +14,50 @@ async fn main() {
         tokio::spawn(async {
             handle_connection(stream).await;
         });
+    }
+}
+
+async fn handle_connection(stream: TcpStream) {
+    let mut handler = resp::RespHandler::new(stream);
+    println!("accepted new connection");
+
+    loop {
+        let value = handler.read_value().await.unwrap();
+
+        println!("Got value {:?}", value);
+
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap(); 
+            match command.as_str() {
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
+                c => panic!("Cannot handle command {}", c)
+            }
+        } else {
+            break;
+        };
+
+        println!("Sending value {:?}", response);
+
+        handler.write_value(response).await.unwrap();
+    }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
+    match value {
+        Value::Array(a) => {
+            Ok((
+                unpack_bulk_str(a.first().unwrap().clone())?,
+                a.into_iter().skip(1).collect(),
+            ))
+        }
+        _ => Err(anyhow::anyhow!("Unexpected command format")),
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("Expected command to be a bulk string"))
     }
 }
